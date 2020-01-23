609b9679f3816e6071ca7e095738654fd6dd1dd5
Michelle Wong <michellekwong2@gmail.com>
2020-01-23 12:17:51 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,34 @@
 // add code snippets from README
+
+let state;
+
+function reducer(state={count: 0},action){
+    switch(action.type){
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1};
+        default:
+            return state;
+    }
+}
+
+function dispatch(action){
+    state= reducer(state,action);
+    render();
+}
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent= state.count
+}
+
+dispatch({type: '@@INIT'})
+
+
+//writing vanilla JS event listener
+//find the button
+let button = document.getElementById('button')
+
+//add event listener on button so on click, dispatch runs
+button.addEventListener('click', ()=>{
+    dispatch({type: 'INCREASE_COUNT'})
+})
